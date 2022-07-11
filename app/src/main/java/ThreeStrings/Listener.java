f8326cea61ff9016b20549bcd33ad5b66fd783c2
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-07-10 21:34:30 -0500

    Version 1.4 Inprog: created voice listener class

--- app/src/main/java/ThreeStrings/Listener.java
+++ app/src/main/java/ThreeStrings/Listener.java
@@ -4,18 +4,13 @@
 package ThreeStrings;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import me.duncte123.botcommons.BotCommons;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.List;
 public class Listener  extends ListenerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class); //implement the Logger class to get rid of error messages
     private final CommandManager manager; //implement our command manager class
@@ -42,24 +37,6 @@ public class Listener  extends ListenerAdapter {
         }
         if (raw.startsWith(prefix)){  //when a command is executed this if statements tells the command manager class to handle it
             manager.handle(event);
-        }
-    }
-    //Disconnect bot if no users is in vc from disconnecting from server
-    @Override
-    public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event){
-        VoiceChannel channel = event.getChannelLeft();
-        List<Member> membersInVc = channel.getMembers();
-        if(membersInVc.size() == 1 && membersInVc.get(0).getUser().isBot()){
-            event.getGuild().getAudioManager().closeAudioConnection();
-        }
-    }
-    //Disconnect bot if no users is in vc from moving to another channel
-    @Override
-    public void onGuildVoiceMove(@NotNull GuildVoiceMoveEvent event){
-        VoiceChannel channel = event.getChannelLeft();
-        List<Member> membersInVc = channel.getMembers();
-        if(membersInVc.size() == 1 && membersInVc.get(0).getUser().isBot()){
-            event.getGuild().getAudioManager().closeAudioConnection();
         }
     }
     }
