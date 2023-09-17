76e7eb5146192b33502aa99e543b54d5d9efacba
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-09-16 22:50:23 -0500

    Version 1.4 Inprog: fixed project structure so that it can app can be ran.

---
+++ app/src/main/java/ThreeStrings/Main.java
@@ -0,0 +1,20 @@
+//Vincent Banks
+//Main Class
+//COPYRIGHT Vincent Banks
+package ThreeStrings;
+
+import ThreeStrings.Bot.Runner;
+
+public final class Main {
+    /*
+    -------THREESTRINGS MAIN METHOD--------
+    This class must never be touched nor modified.
+    all JDABuilder manipulation must be done in Runner class.
+    ----------------------------------------
+     */
+    private Main(){}
+
+    public static void main(String[] args) {
+        Runner.run();
+    }
+}
