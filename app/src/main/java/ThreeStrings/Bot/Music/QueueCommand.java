9b42af6aebb868886c0b4551d076e6ca4de62c84
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-10-22 21:22:06 -0500

    Version 1.4 Inprog: fixed queue command to jda5 standards

--- app/src/main/java/ThreeStrings/Bot/Music/QueueCommand.java
+++ app/src/main/java/ThreeStrings/Bot/Music/QueueCommand.java
@@ -3,6 +3,7 @@
 //COPYRIGHT Vincent Banks
 package ThreeStrings.Bot.Music;
 
+import ThreeStrings.Bot.ExtendedMethods.ArrayMethods;
 import ThreeStrings.Bot.command.CommandContext;
 import ThreeStrings.Bot.command.ICommand;
 import ThreeStrings.Bot.lavaplayer.GuildMusicManager;
@@ -10,8 +11,6 @@ import ThreeStrings.Bot.lavaplayer.PlayerManager;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
-import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
@@ -32,31 +31,21 @@ public final class QueueCommand implements ICommand {
         final int trackCount = Math.min(queue.size(), 20); //takes the smallest number of tracks and sets them as the track count variable (max 20)
         final List<AudioTrack> trackList = new ArrayList<>(queue);
 
-        final MessageCreateAction messageAction = channel.sendMessage(("**Coming up:**\n"));
+        ArrayList<String> queueList = new ArrayList<>();
 
         for (int i = 0; i < trackCount; i++) {
             final AudioTrack track = trackList.get(i);
             final AudioTrackInfo info = track.getInfo();
 
-            messageAction.applyData(MessageCreateData.fromContent("#"))
-                    .applyData(MessageCreateData.fromContent(String.valueOf(i + 1)))
-                    .applyData(MessageCreateData.fromContent(" `"))
-                    .applyData(MessageCreateData.fromContent(info.title))
-                    .applyData(MessageCreateData.fromContent(" by "))
-                    .applyData(MessageCreateData.fromContent(info.author))
-                    .applyData(MessageCreateData.fromContent("` [`"))
-                    .applyData(MessageCreateData.fromContent(formatTime(track.getDuration())))
-                    .applyData(MessageCreateData.fromContent("`]\n"));
+            queueList.add(String.format("%d.  `%s` by `%s` - `%s`  \n",i+1, info.title, info.author, formatTime(track.getDuration() ) ) );
 
         }
 
         if (trackList.size() > trackCount) { //if track list size is bigger than 20
-            messageAction.applyData(MessageCreateData.fromContent("And `"))
-                    .applyData(MessageCreateData.fromContent(String.valueOf(trackList.size() - trackCount)))
-                    .applyData(MessageCreateData.fromContent("` more..."));
+            queueList.add(String.format("and `%s` more...",trackList.size() - trackCount));
         }
 
-        messageAction.queue(); //sends messageAction based upon conditions
+        channel.sendMessageFormat("**Coming up:**\n%s", ArrayMethods.arrayAsString(queueList)).queue();
     }
 
     private String formatTime(long timeInMillis) {
